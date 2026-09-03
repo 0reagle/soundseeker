a5807123e14fd5cefd760595dd2983b7d3c188a9
Silver <53877049+0reagle@users.noreply.github.com>
2026-09-03 20:22:08 +0100

    Update microphone-threshold-test.ino
    Added comments
    Separated sound detected and response with a identifier
    Clarified constants

--- code/microphone-threshold-test.ino
+++ code/microphone-threshold-test.ino
@@ -1,5 +1,7 @@
-int threshold = 1500;
-int micPin = 32;
+const int threshold = 1500;                                    //Threshold for detecting Sound
+const int micPin = 32;                                         // Microphone pin
+bool soundDetected = false;                                    //Sound Detection
+const unsigned long sampleWindow = 50;                                   
 
 void setup() {
   pinMode(micPin, INPUT);
@@ -9,10 +11,10 @@ void setup() {
 
 void loop() {
   int soundLevel;
-  int lowest = 4095;
-  int highest = 0;
-  unsigned long startTime = millis();
-  while (millis() - startTime < 50){
+  int lowest = 4095;                                           //Lowest Value currently read
+  int highest = 0;                                             //Highest Value currently read
+  unsigned long startTime = millis();                          //Gets the current time since the board began to run
+  while (millis() - startTime < sampleWindow){                 //Collect readings for 50 ms and track the lowest and highest values
     int value = analogRead(micPin);
     if (value < lowest){
       lowest = value;
@@ -23,10 +25,17 @@ void loop() {
   }
   soundLevel = highest-lowest;
   if (soundLevel > threshold){
+    soundDetected = true;
+  }
+  else{
+    soundDetected = false;
+  }
+
+  if (soundDetected){
     Serial.println(String("Sound level: ") + soundLevel + " - Sound Detected");
   }
   else{
     Serial.println(String("Sound level: ") + soundLevel + " - No Sound");
   }
-  delay(100);
+  delay(100);                                                                     //Short delay to make the serial output easier to read
 }
